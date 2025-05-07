2ec63cc9f2470907e0802cf23d3d2ca9f81293f9
keinerst7 <keinerarenas77@gmail.com>
2025-05-07 07:42:15 -0500

    szs porquesi

--- src/index.ts
+++ src/index.ts
@@ -10,13 +10,58 @@ app.use(bodyParser.json());
 app.use(express.json());
 const prisma = new PrismaClient()
 
-app.get('/contact', async (req: Request, res: Response) => {
-    //const stage = req.query.stage;
-    const a = res.header("api_key", "hola")
-    console.log(a);
-    const contact = await prisma.contact.findMany();
-    res.status(200).json({"ms": contact});
+
+/////////////////////////////// RUTAS CONTACTS ////////////////////
+async function main() {
+  // Crear nueva empresa
+  const newCompany = await prisma.company.create({
+    data: {
+      name: 'IA WORLD',
+      industry: 'Clean IA',
+      website: 'https://iaclean.com',
+      address: 'Calle Amarilla 35',
+      phone: '222-333-111',
+      status: 'active',
+    },
   });
+
+  console.log('Empresa creada:', newCompany);
+
+  // Crear nuevo contacto asociado
+  const newContact = await prisma.contact.create({
+    data: {
+      firstName: 'Leon',
+      lastName: 'Diaz',
+      email: 'juan.diaz@iaclean.com',
+      phone: '333-456-1222',
+      position: 'Office Manager',
+      status: 'active',
+      companyId: newCompany.id,
+    },
+  });
+
+  console.log('Contacto creado:', newContact);
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
+
+  
+
+
+// Ruta Obtener todos los datos de contact
+app.get('/contact', async (req: Request, res: Response) => {
+  //const stage = req.query.stage;
+  const a = req.header("api_key")
+  const contact = await prisma.contact.findMany();
+  res.status(200).json({"ms": contact, "header": a});
+});
+
 
 
 // Ruta para buscar un contacto por ID
@@ -31,8 +76,9 @@ app.get('/contact/:id', async (req: Request, res: Response) => {
       res.status(500).json({ error: 'Error al buscar el contacto.' });
     }
   });
-  
 
+
+/////////////////////////////// RUTAS OPPORTUNITY ///////////////////
 // Crear una nueva oportunidad
 app.post('/Opportunity', async (req: Request, res: Response) => {
   const { name, description, estimatedValue, currency, stage, expectedCloseDate, companyId } = req.body;
